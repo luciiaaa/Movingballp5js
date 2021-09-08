eadeed5234b15ea7f38356805a018a85c202e956
LuciaCaro1 <74767567+luciiaaa@users.noreply.github.com>
2021-09-08 10:41:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+  var x, y, v;
+
+function setup() {
+	createCanvas(500, 400);
+
+  x = 0;
+  y = 200;
+  v = 5;
+}
+
+function draw() {
+	background(225);
+  
+  ellipse(x,y,50,50);
+  x = x + v;
+
+  
+
+  if(x <= 0 || x > 400){
+    v = v * -1;
+  }
+}
+	
