b7f3a7ca8d54773ede12743178972f875c2007e0
Lucia Caro <74767567+luciiaaa@users.noreply.github.com>
2021-09-08 10:51:24 +0000

    verticaal balletje

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 
-  var x, y, v;
+  var x, y, v ;
 
 function setup() {
 	createCanvas(500, 400);
 
   x = 0;
-  y = 200;
+  y = 0;
   v = 5;
+ 
 }
 
 function draw() {
@@ -15,11 +16,17 @@ function draw() {
   
   ellipse(x,y,50,50);
   x = x + v;
+  y = y + x;
 
   
 
   if(x <= 0 || x > 400){
     v = v * -1;
   }
+
+  
+
+
+  
 }
 	
