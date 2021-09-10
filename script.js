fab3adfa54e312a3ce2c51570b301bfac9a3c2c2
Lucia Caro <74767567+luciiaaa@users.noreply.github.com>
2021-09-10 08:21:15 +0000

    bal stuitert goed

--- script.js
+++ script.js
@@ -5,9 +5,10 @@
 function setup() {
 	createCanvas(500, 400);
 
-  x = 0;
-  y = 0;
+  x = 55;
+  y = 200;
   v = 5;
+  z= 5;
  
 }
 
@@ -16,14 +17,18 @@ function draw() {
   
   ellipse(x,y,50,50);
   x = x + v;
-  y = y + x;
+  y = y + z;
 
   
 
-  if(x <= 0 || x > 400){
+  if(x <= 0 || x > 500){
     v = v * -1;
   }
 
+ if(y <= 0 || y > 400){
+    z = z * -1;
+ }
+  
   
 
 
