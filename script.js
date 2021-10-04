442ea68429ea3ff93a173d1a0a61f49182b49fc1
Lucia Caro <74767567+luciiaaa@users.noreply.github.com>
2021-10-04 13:32:42 +0000

    gekke dingen

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let paddle_height = 35;
 let ball_x = 100;
 let ball_y = 100;
 let speed = 3;
+let rect1_height = 70;
+let rect1_width = 20;
 
 var cx, px, cy, py, cv, pv;
 
@@ -48,6 +50,7 @@ function draw() {
   
   paddle();
   ballBounce ();
+  rect1();
   
   
 
@@ -71,12 +74,16 @@ function ballBounce() {
   if(ball_y >= height || ball_y <= 0){
     directionY = -directionY
   }
+
+  
+
+
+  
  
   
 }
 
   
-
 function paddle() {
   if (ball_y < paddle_height &&
     ball_x > mouseX - paddle_width / 2 &&
@@ -88,4 +95,17 @@ function paddle() {
   
   rect(mouseX, paddle_height / 2, paddle_width, paddle_height);
  
+}
+
+
+function rect1(){
+  if (px < rect1_height &&
+    py > mouseX - rect1_width / 2 &&
+    py < mouseX + rect1_width / 2) {
+    directionX = -directionX
+    
+  }
+
+  
+  rect(mouseX, rect1_width / 2, rect1_height, rect1_width);
 }
