29b060273d6fb1130792763a95807356e2173280
Lucia Caro <74767567+luciiaaa@users.noreply.github.com>
2021-10-08 08:34:22 +0000

    goede versie

--- script.js
+++ script.js
@@ -1,46 +1,91 @@
 
-class Player{
-  constructor (x, y, w, h){
-    this.x = x;
-    this.y = y;
-    this.width = w;
-    this.height = h;
-  }
-
-  draw(){
-    ellipse(this.x,this.y,50,50);
-    this.x = this.x + this.vx;
-    this.y = this.y + this.vy;
-
-    if(this.x < 25 || this.x >= 475){
-      this.vx = this.vx * -1;
-    }
-
-    if(this.y < 25 || this.y >= 375){
-      this.vy = this.vy * -1;
-    }
-  }
-
-}
 
 
-var ball1, ball2, ball3;
+let directionX = 1.5;
+let directionY = 1;
+let paddle_width = 100; 
+let paddle_height = 35;
+let ball_x = 100;
+let ball_y = 100;
+let speed = 3;
+
+var cx, px, cy, py, cv, pv;
+
 
 function setup() {
-	createCanvas(500, 400);
+	createCanvas(600, 400)
+  rectMode(CENTER)
 
-  ball1 = new Ball(30,200,50,50,5,5);
-
-  ball2 = new Ball(100,100,50,50,10,10);
-
-  ball3 = new Ball(200,150,50,50,1,-1);
-
+  cx = 20;
+  cy = 200;
+  cv = 2;
+  px = 560;
+  py = 200;
+  pv = 2;
 }
 
 function draw() {
-	background(225);  
+	background(0);
+
+  let a = color('green')
+  rect(cx, cy, 20, 70);
+  fill(a)
+  cy = cy + cv
+ 
+
+  if(cy < 0 || cy > 350) {
+   cv = cv * -1;
+  }
+
+
+  rect(mouseX, mouseY, 20, 70);
+  fill(a)
+  py = py + pv
+
+  if(py < 0 || py > 350) {
+   pv = pv * -1;
+  }
   
-  ball1.draw();
-  ball2.draw();
-  ball3.draw();
+  paddle();
+  ballBounce ();
+  
+  
+
+  
+
+}
+
+
+
+
+function ballBounce() {
+
+  ellipse(ball_x, ball_y, 20, 20);
+  ball_x += directionX * speed ;
+  ball_y += directionY * speed;
+
+   if (ball_x >= width || ball_x <= 0) {
+    directionX = -directionX
+  }
+
+  if(ball_y >= height || ball_y <= 0){
+    directionY = -directionY
+  }
+ 
+  
+}
+
+  
+
+function paddle() {
+  if (ball_y < paddle_height &&
+    ball_x > mouseX - paddle_width / 2 &&
+    ball_x < mouseX + paddle_width / 2) {
+    directionY = -directionY
+    
+  }
+
+  
+  rect(mouseX, paddle_height / 2, paddle_width, paddle_height);
+ 
 }
